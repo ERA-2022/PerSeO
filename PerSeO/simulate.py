80e621056abac06d39def8c6a3988c36f32f28dc
J4ngel <andres_angel@live.com.ar>
2024-06-06 23:19:23 -0500

    docs: :memo: Se documentan las funciones del modulo simulate

--- PerSeO/simulate.py
+++ PerSeO/simulate.py
@@ -20,14 +20,21 @@ from .graphicsManagement import draw_one_report
 
 
 def create_sim_file(particle: np.ndarray, i: int, j: int):
+    """Create a Python script that can be executed from Ansys HFSS with the necessary instructions to open the model, modify the array in the model dimensions, generate the required reports and close the program.
+
+    Args:
+        particle (np.ndarray): numpy ndarray type, contains the dimensions to be placed in the Ansys model
+        i (int): Number of iteration
+        j (int): Number of particle
+    """
     required_reports = read_data()['values']['reports']
     particle = particle.round(4)
 
-    tag = "_" + str(i) + "_" + str(j)  # esta linea genera el string _i_j
-    var = "[" + ", ".join([str(x) for x in particle]) + "]"  # Generar string del array de datos de la partícula
+    tag = "_" + str(i) + "_" + str(j)
+    var = "[" + ", ".join([str(x) for x in particle]) + "]"
 
-    f = open(read_data()['paths']['src'] + "simulacion.py", "w")  # abre un archivo para escribir
-    direccion_dibujo = '"' + read_data()["paths"]["ansys_save_def"] + read_data()["values"]["project_name"] + '.aedt"'
+    f = open(read_data()['paths']['src'] + "simulacion.py", "w")
+    pathProject = '"' + read_data()["paths"]["ansys_save_def"] + read_data()["values"]["project_name"] + '.aedt"'
 
     f.write("# -*- coding: utf-8 -*-\n")
     f.write("import PerSeO.ansys_functions as fn\n")
@@ -35,7 +42,7 @@ def create_sim_file(particle: np.ndarray, i: int, j: int):
     f.write("\n")
     f.write("import ScriptEnv\n")
     f.write("oDesktop.RestoreWindow()\n")
-    f.write("oDesktop.OpenProject(" + direccion_dibujo + ")\n")
+    f.write("oDesktop.OpenProject(" + pathProject + ")\n")
     f.write("oProject = oDesktop.SetActiveProject(" + '"' + read_data()['values']['project_name'] + '"' + ")\n")
 
     f.write(
@@ -60,6 +67,16 @@ def create_sim_file(particle: np.ndarray, i: int, j: int):
 
 ## Launches HFSS simulation file
 def run_simulation_hfss(ansys_path="", args='-runscriptandexit', file_path=""):
+    """Open Ansys HFSS and from that program run the script 'simulacion.py', if the simulation runs correctly it returns true, otherwise false.
+
+    Args:
+        ansys_path (str, optional): path where the Ansys HFSS executable is located. Defaults read ./src/data.json and take ansys_exe value.
+        args (str, optional): arguments to be run by the subprocess to open ansys. Defaults to '-runscriptandexit'.
+        file_path (str, optional): path where the simulation script is located. Defaults read ./src/data.json and take src value + simulacion.py.
+
+    Returns:
+        bool: returns true or false depending on whether it was possible to open Ansys and run the script 'simulacion.py' correctly.
+    """
     if ansys_path == "":
         ansys_path = read_data()['paths']['ansys_exe']
 
@@ -67,23 +84,33 @@ def run_simulation_hfss(ansys_path="", args='-runscriptandexit', file_path=""):
         file_path = read_data()['paths']['src'] + "simulacion.py"
 
     state = True
-    Econt = 0
-    while state and Econt < 22:
+    counterError = 0
+    while state and counterError < 22:
         state = bool(subprocess.run([ansys_path, args, file_path]).returncode)
         if state:
             logging.info(msg.SIM_PARTICLE_FINISHED + msg.HAD_AN_ERR)
-            print(msg.EXE_P1_ERR + str(Econt + 1) + msg.EXE_P2_ERR)
-            if Econt < 21:
-                time.sleep(3 + Econt)
-            Econt += 1
+            print(msg.EXE_P1_ERR + str(counterError + 1) + msg.EXE_P2_ERR)
+            if counterError < 21:
+                time.sleep(3 + counterError)
+            counterError += 1
         else:
             logging.info(msg.SIM_PARTICLE_FINISHED + msg.NO_ERR)
-    #print("Intentos: "+str(Econt))
+    #print("Attempts: "+str(counterError))
     return state
 
 
 ## read the simulation results
-def read_simulation_results(i, j, graph):
+def read_simulation_results(i: int, j: int, graph: bool):
+    """Reads the reports of a particle in one iteration, and returns them in a dictionary, optionally graphs them.
+
+    Args:
+        i (int): Number of iteration
+        j (int): Number of particle
+        graph (bool): boolean value representing whether it is necessary to plot the read data
+
+    Returns:
+        dict: Returns a dictionary with the data read, where each key is the name of the report read, example S11, VSWR
+    """
     dataReports = {}
     required_reports = read_data()['values']['reports']
 
@@ -180,6 +207,11 @@ def read_simulation_results(i, j, graph):
 
 
 def get_simulation_params():
+    """reads data from the ./src/data.json file and returns a dictionary with branches, iterations, particles, dimension array size, nominal value array, minimum maxima and description.
+
+    Returns:
+        dict: dictionary with branches, iterations, particles, dimension array size, nominal value array, minimum maxima and description.
+    """
     info = read_data()
 
     if info['info']['category'].upper() == "ANTENNA":
@@ -237,8 +269,13 @@ def create_plot_imb(data_1, label_x, label_y, save_path, boundary):
     plt.close(figure)
 
 
-def copy_rename(old_file_name, new_file_name):
+def copy_rename(old_file_name: str, new_file_name: str):
+    """Takes an existing file from the /results/ folder of the optimization id found in data.json and creates a copy with a new name in the same path
 
+    Args:
+        old_file_name (str): Existing file name
+        new_file_name (str): Name of the new file
+    """
     files_location = os.path.join(
         os.path.normpath(read_data()['paths']['results']),
         read_data()['info']['ID'], r"files"
@@ -251,6 +288,11 @@ def copy_rename(old_file_name, new_file_name):
 
 
 def init_model():
+    """Verify that the model (.aedt file) exists, otherwise it will try to run a script with the model name in the ./models/ folder. At the end it returns true or false if the model is found.
+
+    Returns:
+        bool: returns true if the model exists
+    """
     error = False
     data = read_data()
 
