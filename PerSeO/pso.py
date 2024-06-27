a10d387421a67c5708f2d44710632c262529566b
J4ngel <andres_angel@live.com.ar>
2024-06-27 11:46:50 -0500

    refactor: :recycle: Se reorganizan de mejor manera las clases, se agrega mas documentación de las mismas y se eliminan linas de código que no fueron implementadas

--- PerSeO/pso.py
+++ PerSeO/pso.py
@@ -24,7 +24,6 @@ class Particle:
             Assigns 0 to the class variable id_.
     """
     id_ = 0
-    values_array = []
 
     def __init__(self, id: int):
         """Initializes a new particle
@@ -33,12 +32,13 @@ class Particle:
             id (int): identifier
         """
         self.id_ = id
+        self.values_array = []
 
     def random_array(self, array_size: int):
         """Assigns to the values_array attribute a Numpy array (ndarray) of random values between 0 and 1, the number of array elements will be determined by the argument passed in the function.
 
         Args:
-            array_size (int): _description_
+            array_size (int): array size
         """
         self.values_array = np.array(np.random.random_sample(array_size))
 
@@ -46,7 +46,7 @@ class Particle:
         """Assigns to the values_array attribute a Numpy array (ndarray), the number of array elements will be determined by the argument passed in the function and each element will be zero.
 
         Args:
-            array_size (int): _description_
+            array_size (int): array size
         """
         self.values_array = np.zeros(array_size)
 
@@ -61,6 +61,9 @@ class Swarm:
 
     Attributes:
         phiv(float): Swarm inertia coefficient
+        phi1(float): acceleration coefficient of the cognitive component. default 2.0
+        phi2(float): acceleration coefficient of the social component. default 2.1
+        damping(float): this damping is used when the particles touch the maximum and minimum limits. default 0.7
         particles(list): List of all particles that are part of the swarm
         x_particles(list): list to calculate the new particles  (internal use)
         best_index(int): Index of the best particle in the particle list
@@ -69,6 +72,7 @@ class Swarm:
         gbest(float): Best overall value found by the swarm
         pbest(ndarray): Matrix of the best personal/local values of the particles.
         pg(ndarray): Matrix of the best global positions found by the swarm.
+        particles_number (int): Number of particles that the swarm will have
         variables_number(int): Number of variables defining each particle (In the interface provided by PerSeO it is associated to the array of dimensions of the Ansys HFSS model).
         var_max(ndarray): Maximum values that a particle can take
         var_min(ndarray): Minimum values that a particle can take
@@ -78,20 +82,8 @@ class Swarm:
         drilling_relation(self, dimension, height): Calculate a drilling ratio. ((height / 2) * 1 / dimension)
         get_particle_best_fit(self, pi): Obtains the best particle fit among all particles.
     """
-    phiv = 0.9
 
-    particles = []  # array with all particles part of the swarm
-    x_particles = []  # array with all particles part of the swarm
-    best_index = 0  # best integer describing the index corresponding the best particle in
-    # particles array
-    vmax = []
-    velocities = []
-    gbest = 0  # global  best
-    pbest = []
-    pg = []
-    variables_number = 0
-
-    def __init__(self, particles_number: int, variables_number: int, var_max: list | np.ndarray, var_min: list | np.ndarray):
+    def __init__(self, particles_number: int, variables_number: int, var_max: list | np.ndarray, var_min: list | np.ndarray, phiv: float = 0.9, phi1: float = 2.0, phi2: float = 2.1, damping: float = 0.7):
         """Initializes a new swarm
 
         Args:
@@ -99,16 +91,27 @@ class Swarm:
             variables_number (int): Number of variables defining each particle (In the interface provided by PerSeO it is associated to the array of dimensions of the Ansys HFSS model).
             var_max (list | ndarray): Maximum values that a particle can take
             var_min (list | ndarray): Minimum values that a particle can take
+            phiv(float): Swarm inertia coefficient. default 0.9
+            phi1(float): acceleration coefficient of the cognitive component. default 2.0
+            phi2(float): acceleration coefficient of the social component. default 2.1
+            damping(float): this damping is used when the particles touch the maximum and minimum limits. default 0.7
         """
-        # swarm variables
+        
+        self.phiv = phiv
+        self.phi1 = phi1
+        self.phi2 = phi2
+        self.damping = damping
+
+        self.particles = []
+        self.x_particles = []
         self.particles_number = particles_number
         self.variables_number = variables_number
-
-        # Set variations
+        self.best_index = 0
 
         self.var_max = np.array(var_max)
         self.var_min = np.array(var_min)
-        self.particles = []
+        self.vmax = []
+        self.gbest = 0
         self.velocities = np.zeros([read_data()['values']['particles'], read_data()['values']['n_var']])
         self.pbest = np.zeros(read_data()['values']['particles'])
         self.pg = np.zeros(read_data()['values']['n_var'])
@@ -130,11 +133,6 @@ class Swarm:
             # Scale the random values
             particle.values_array = particle.values_array * (self.var_max - self.var_min) + self.var_min
 
-            #  if particle.values_array[2] < particle.values_array[1]:
-
-            #      random = np.random.random_sample()
-            #      maximum = particle.values_array[1]-0.1
-            #      particle.values_array[2] = random*(maximum-self.var_min[2]) + self.var_min[2]
 
     def calculate_new_particles(self, previous_particles: list, pi_best: list, pg: np.ndarray, vel_anterior: np.ndarray, iteration: int):
         """Generate new particles based on previous particles, personal and global best values, previous velocities and current iteration. At the end it returns a tuple of two elements, the first one is a list with the new calculated particles and the second one is a ndarray with the new calculated velocities.
@@ -158,12 +156,8 @@ class Swarm:
 
         # Dynamic change of inertia
         phi = 0.85 * self.phiv**(iteration - 0.15)  # 0.8 y 1
-        phi1 = 2.0  # values that can be reviewed. Follow the value of your own best fit
-        phi2 = 2.1  # this goes values make up self-knowledge. follow the best global fit
-        damping = 0.7  # This damping is used when the particles touch the maximum and minimum limits.
 
         for i in range(read_data()['values']['particles']):
-
             rand = np.random.random()  # values between 0 and 1
             previous_particle = previous_particles[i]
 
@@ -184,49 +178,39 @@ class Swarm:
                 inertia + attraction to best position of particle i + attraction to best global position
                 """
 
-                vel[i][idx] = phi * (vel_anterior[i][idx]) + phi1 * rand * ((pi_best[i].values_array)[idx] - dimension) + \
-                     phi2 * rand * (pg[idx] - dimension)
+                vel[i][idx] = phi * (vel_anterior[i][idx]) + self.phi1 * rand * ((pi_best[i].values_array)[idx] - dimension) + self.phi2 * rand * (pg[idx] - dimension)
 
                 # Limiting velocity when too large
                 if np.abs(vel[i][idx]) > self.vmax[idx]:
-                    signo = np.sign(vel[i][idx])
-                    vel[i][idx] = self.vmax[idx] * signo
+                    sign = np.sign(vel[i][idx])
+                    vel[i][idx] = self.vmax[idx] * sign
 
-                # Calculating new particles
+                # Calculating new particles  xi(t-1)+vi(t)
                 self.x_particles[i].values_array[idx] = (previous_particles[i].values_array[idx] + vel[i][idx]).round(
                     decimals=3, out=None
-                )  # #  xi(t-1)+vi(t)
+                )
                 """In this part we apply a bounce technique in the wall defined
                 by the limits of max and min values for dimensions"""
                 if self.x_particles[i].values_array[idx] > self.var_max[idx]:
 
-                    vel[i][idx] = vel[i][idx] * damping
+                    vel[i][idx] = vel[i][idx] * self.damping
 
                     self.x_particles[i].values_array[idx] = (self.var_max[idx] - np.abs(vel[i][idx])).round(
                         decimals=3, out=None
-                    )  # #  xi(t-1)+vi(t)
+                    )
 
                 elif self.x_particles[i].values_array[idx] < self.var_min[idx]:
 
-                    vel[i][idx] = vel[i][idx] * damping
+                    vel[i][idx] = vel[i][idx] * self.damping
 
                     self.x_particles[i].values_array[idx] = (self.var_min[idx] + np.abs(vel[i][idx])).round(
                         decimals=3, out=None
-                    )  # #  xi(t-1)+vi(t)
+                    )
 
                 else:
                     self.x_particles[i].values_array[idx] = (previous_particles[i].values_array[idx] + vel[i][idx]).round(
                         decimals=3, out=None
-                    )  # #  xi(t-1)+vi(
-
-                # This sections is used to force a=B*2
-                # if idx==global_.A_dimension_index+1:
-                #   self.x_particles[i].values_array[idx] = (self.x_particles[i].values_array[idx-1])*0.5
-
-            #  PENDING -> ONLY FOR HYBRIDS
-            #  if i>0:
-            #      relation = self.drilling_relation( self.x_particles[i].values_array, self.x_particles[i].values_array[global_.A_dimension_index])
-            #      print(relation)
+                    )
 
         return self.x_particles, vel
 
